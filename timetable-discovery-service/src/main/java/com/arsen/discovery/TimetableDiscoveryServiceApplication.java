d2f0f79fc32b7b8f39ce265be09f96c97c710909
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 16:04:57 +0200

    TIMETABLE-31-2 Fixed discovery server

--- timetable-discovery-service/src/main/java/com/arsen/discovery/TimetableDiscoveryServiceApplication.java
+++ timetable-discovery-service/src/main/java/com/arsen/discovery/TimetableDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.arsen.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TimetableDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
