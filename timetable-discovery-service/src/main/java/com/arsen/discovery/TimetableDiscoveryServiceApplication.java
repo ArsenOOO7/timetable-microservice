dee94107042b94d3731bdc669e2b57b98844d3d6
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 13:51:29 +0200

    TIMETABLE-4 Add discovery service

---
+++ timetable-discovery-service/src/main/java/com/arsen/discovery/TimetableDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arsen.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ timetable-discovery-service/src/test/java/com/arsen/discovery/TimetableDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
