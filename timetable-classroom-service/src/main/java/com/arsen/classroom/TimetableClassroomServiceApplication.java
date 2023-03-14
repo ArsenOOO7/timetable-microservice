bc8a2f4b573fddd556d2bf6d559e0809a29af2ec
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 13:57:54 +0200

    TIMETABLE-7 Add classroom service

---
+++ timetable-classroom-service/src/main/java/com/arsen/classroom/TimetableClassroomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arsen.classroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableClassroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableClassroomServiceApplication.class, args);
+	}
+
+}
---
+++ timetable-classroom-service/src/test/java/com/arsen/classroom/TimetableClassroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.classroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableClassroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
