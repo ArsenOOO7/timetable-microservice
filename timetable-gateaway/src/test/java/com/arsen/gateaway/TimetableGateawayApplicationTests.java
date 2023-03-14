e0a06ab83bad5f5d391278c8c7ed396c248b7bd0
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 13:59:00 +0200

    TIMETABLE-8 Add gateaway service

---
+++ timetable-gateaway/src/test/java/com/arsen/gateaway/TimetableGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
