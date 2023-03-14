4ef5fdcf6ef98d731ea67b478cbb0ab9fa7acd07
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 13:56:50 +0200

    TIMETABLE-6 Add timetable service

---
+++ timetable-timetable-service/src/test/java/com/arsen/timetable/TimetableTimetableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableTimetableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
