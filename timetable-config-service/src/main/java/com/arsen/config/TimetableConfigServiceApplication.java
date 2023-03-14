f7f333ce1d3edcd0094b036af7aec5e0ff94a2d4
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 13:50:04 +0200

    TIMETABLE-3 Add config service

---
+++ timetable-config-service/src/main/java/com/arsen/config/TimetableConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arsen.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableConfigServiceApplication.class, args);
+	}
+
+}
---
+++ timetable-config-service/src/test/java/com/arsen/config/TimetableConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
