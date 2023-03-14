2739b8ee8eca8fec6f56b51b035039e37eb92bf3
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-14 13:52:40 +0200

    TIMETABLE-5 Add subject service

---
+++ timetable-subject-service/src/test/java/com/arsen/subject/TimetableSubjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableSubjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
