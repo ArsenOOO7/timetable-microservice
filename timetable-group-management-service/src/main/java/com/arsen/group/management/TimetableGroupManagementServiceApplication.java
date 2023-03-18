1953680ee02632570ec94b81eb79de797e38b01e
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-18 17:31:39 +0200

    TIMETABLE-35 Create group management service

---
+++ timetable-group-management-service/src/main/java/com/arsen/group/management/TimetableGroupManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arsen.group.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableGroupManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableGroupManagementServiceApplication.class, args);
+	}
+
+}
