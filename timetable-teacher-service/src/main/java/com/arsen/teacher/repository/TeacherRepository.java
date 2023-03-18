2a3286c39d780eb518bccf9a4866d8b7950861b2
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-18 16:59:54 +0200

    TIMETABLE-34 Add read operation to the teacher

--- timetable-teacher-service/src/main/java/com/arsen/teacher/repository/TeacherRepository.java
+++ timetable-teacher-service/src/main/java/com/arsen/teacher/repository/TeacherRepository.java
@@ -1,6 +1,7 @@
 package com.arsen.teacher.repository;
 
 import com.arsen.teacher.domain.Teacher;
+import com.arsen.teacher.dto.TeacherDto;
 import com.arsen.teacher.dto.TeacherResultSearchDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
@@ -18,5 +20,8 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
             "or lower(t.fatherName) like lower(concat('%', :query, '%')) " +
             "or lower(concat(t.firstName, ' ', t.lastName, ' ', t.fatherName)) like lower(concat('%', :query, '%'))")
     List<TeacherResultSearchDto> findAllByQuery(@Param("query") String query);
+
+    @Query("select new com.arsen.teacher.dto.TeacherDto(t.id, t.firstName, t.lastName, t.fatherName, t.email, t.meetingLink) from Teacher t where t.id = :id")
+    Optional<TeacherDto> findDtoById(@Param("id") long id);
 
 }
