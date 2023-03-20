a2bc3e7c0046470db40d07a52c78baece6931561
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-03-20 10:38:37 +0200

    TIMETABLE-47-3 Add new requests to Group Management

--- timetable-timetable-service/src/main/java/com/arsen/timetable/client/GroupManagementClient.java
+++ timetable-timetable-service/src/main/java/com/arsen/timetable/client/GroupManagementClient.java
@@ -3,13 +3,15 @@ package com.arsen.timetable.client;
 
 import com.arsen.timetable.dto.group.GroupLessonDto;
 import com.arsen.timetable.dto.group.GroupLessonsRequestDto;
-import com.arsen.timetable.dto.group.GroupLessonsResponseDto;
 import com.arsen.timetable.dto.group.MultipleGroupLessonDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Set;
 
 @FeignClient(name = "group-management")
 public interface GroupManagementClient {
@@ -18,7 +20,10 @@ public interface GroupManagementClient {
     MultipleGroupLessonDto readByLesson(@PathVariable long lesson);
 
     @PostMapping("/api/group/management/group/{groupId}")
-    GroupLessonsResponseDto readByGroup(@PathVariable long groupId, @RequestBody GroupLessonsRequestDto requestDto);
+    List<MultipleGroupLessonDto> readByGroup(@PathVariable long groupId, @RequestBody GroupLessonsRequestDto requestDto);
+
+    @PostMapping("/api/group/management/lesson")
+    List<MultipleGroupLessonDto> readByGroup(@RequestBody Set<Long> ids);
 
     @PostMapping("/api/group/management")
     void create(@RequestBody GroupLessonDto lessonDto);
