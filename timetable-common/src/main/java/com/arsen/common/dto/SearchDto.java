c63d79459e701aa81aa0d259e0a69fd6b3c3a199
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-04-15 13:48:23 +0300

    TIMETABLE-58 Add new DTO

---
+++ timetable-common/src/main/java/com/arsen/common/dto/SearchDto.java
@@ -0,0 +1,13 @@
+package com.arsen.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class SearchDto {
+    private String searchQuery;
+}
