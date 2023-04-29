fad06ec5b62f5d62c3a454db53766cf02a8e6b22
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-04-29 15:08:17 +0300

    TIMETABLE-64 Little refactor timetable

--- timetable-timetable-service/src/main/java/com/arsen/timetable/controller/TimetableController.java
+++ timetable-timetable-service/src/main/java/com/arsen/timetable/controller/TimetableController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 @RestController
 @RequestMapping("/api/timetable")
 @RequiredArgsConstructor
@@ -32,8 +34,8 @@ public class TimetableController {
     }
 
     @PostMapping
-    public LessonCreatedBrieflyResponseDto create(@RequestBody LessonDto lessonDto){
-        return lessonService.create(lessonDto);
+    public ResponseEntity<LessonCreatedBrieflyResponseDto> create(@RequestBody LessonDto lessonDto){
+        return ResponseEntity.status(CREATED).body(lessonService.create(lessonDto));
     }
 
     @PutMapping("/{id}")
