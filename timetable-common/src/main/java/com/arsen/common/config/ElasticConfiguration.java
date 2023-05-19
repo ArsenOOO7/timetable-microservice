4a5f00959894804e4f8788dfb94891ae096085bb
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-05-20 00:08:16 +0300

    [TIMETABLE-82] Add new configuration to connect with Elasticsearch

--- timetable-common/src/main/java/com/arsen/common/config/ElasticConfiguration.java
+++ timetable-common/src/main/java/com/arsen/common/config/ElasticConfiguration.java
@@ -1,4 +1,4 @@
-package com.arsen.group.config;
+package com.arsen.common.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
@@ -8,14 +8,12 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.arsen.group.repository")
-@ComponentScan(basePackages = "com.arsen.group.domain")
-public class ElasticSearchConfiguration{
+@EnableElasticsearchRepositories
+public class ElasticConfiguration {
 
     @Value("${elasticsearch.address}")
     private String elasticAddress;
