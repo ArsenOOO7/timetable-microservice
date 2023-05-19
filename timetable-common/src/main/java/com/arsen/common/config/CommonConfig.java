fe8760e918d40b429f790941b3aa8631653a3f6e
Arsen Sydoryk <30925793+ArsenOOO7@users.noreply.github.com>
2023-05-20 00:10:09 +0300

    [TIMETABLE-82] Move Elasticsearch configuration to common-library (#83)
    * [TIMETABLE-82] Add new configuration to connect with Elasticsearch
    
    * [TIMETABLE-82] Enable Elasticsearch

--- timetable-common/src/main/java/com/arsen/common/config/CommonConfig.java
+++ timetable-common/src/main/java/com/arsen/common/config/CommonConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.arsen.common")
+@ComponentScan(value = "com.arsen.common", excludeFilters = @ComponentScan.Filter(Configuration.class))
 public class CommonConfig {
 }
