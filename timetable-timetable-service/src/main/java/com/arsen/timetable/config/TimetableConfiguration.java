f1ac2230f16cef2d1da5802bfa1b4b0d249c9892
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-05-23 00:37:32 +0300

    [TIMETABLE-91] Add security to timetable service

--- timetable-timetable-service/src/main/java/com/arsen/timetable/config/TimetableConfiguration.java
+++ timetable-timetable-service/src/main/java/com/arsen/timetable/config/TimetableConfiguration.java
@@ -4,6 +4,7 @@ import com.arsen.common.annotation.EnableCommon;
 import com.arsen.timetable.event.ClassroomUpdateEvent;
 import com.arsen.timetable.event.SubjectUpdateEvent;
 import com.arsen.timetable.event.TeacherUpdateEvent;
+import com.arsen.timetable.filter.JwtVerifyFilter;
 import com.arsen.timetable.service.readonly.ClassroomReadService;
 import com.arsen.timetable.service.readonly.SubjectReadService;
 import com.arsen.timetable.service.readonly.TeacherReadService;
@@ -12,6 +13,11 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.function.Consumer;
 
@@ -19,12 +25,14 @@ import java.util.function.Consumer;
 @EnableRabbit
 @EnableCommon
 @EnableDiscoveryClient
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class TimetableConfiguration {
 
     private final ClassroomReadService classroomReadService;
     private final TeacherReadService teacherReadService;
     private final SubjectReadService subjectReadService;
+    private final JwtVerifyFilter jwtVerifyFilter;
 
 
     @Bean
@@ -40,6 +48,18 @@ public class TimetableConfiguration {
     @Bean
     public Consumer<TeacherUpdateEvent> teacherUpdateEventConsumer(){
         return teacherReadService::synchronize;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .addFilterBefore(jwtVerifyFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic()
+                .and().build();
     }
 
 }
