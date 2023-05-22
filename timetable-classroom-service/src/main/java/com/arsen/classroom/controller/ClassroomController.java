7e157b4b9b38c14cc6dabadf561ab1931099ab0f
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-05-23 01:46:38 +0300

    [TIMETABLE-95] Add Swagger for classrooms

--- timetable-classroom-service/src/main/java/com/arsen/classroom/controller/ClassroomController.java
+++ timetable-classroom-service/src/main/java/com/arsen/classroom/controller/ClassroomController.java
@@ -7,7 +7,6 @@ import com.arsen.common.dto.SearchDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
