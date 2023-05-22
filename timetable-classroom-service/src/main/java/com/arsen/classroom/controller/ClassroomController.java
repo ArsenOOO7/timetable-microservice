6c1ac54c62415dd5176730391bc20b9265b2ee67
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-05-22 22:39:34 +0300

    [TIMETABLE-91] Authorization in classroom service

--- timetable-classroom-service/src/main/java/com/arsen/classroom/controller/ClassroomController.java
+++ timetable-classroom-service/src/main/java/com/arsen/classroom/controller/ClassroomController.java
@@ -6,6 +6,7 @@ import com.arsen.classroom.service.ClassroomService;
 import com.arsen.common.dto.SearchDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 @RequestMapping("/api/classroom")
 @RequiredArgsConstructor
+@PreAuthorize(value = "hasAnyRole('ADMIN', 'UNIT_MANAGER')")
 public class ClassroomController {
 
     private final ClassroomService classroomService;
