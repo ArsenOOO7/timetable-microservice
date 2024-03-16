cf052bc06d49fa9aede2f0437b428d1800df461a
JJerome-NM <jjerome010@gmail.com>
2024-03-16 15:54:12 +0200

    [TIMETABLE-112] Setup identity access microservice

--- timetable-config/src/main/java/com/pnu/system/TimetableConfigApplication.java
+++ timetable-config/src/main/java/com/pnu/system/TimetableConfigApplication.java
@@ -2,10 +2,10 @@ package com.pnu.system;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 @EnableConfigServer
 public class TimetableConfigApplication {
     public static void main(String[] args) {
---
+++ timetable-identity-access/src/main/java/com/pnu/system/TimetableIdentityAccessApplication.java
@@ -0,0 +1,12 @@
+package com.pnu.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TimetableIdentityAccessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TimetableIdentityAccessApplication.class, args);
+    }
+}
