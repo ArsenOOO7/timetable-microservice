696de8b0578c78711ed65ee017765a40a9de11e5
JJerome-NM <jjerome010@gmail.com>
2024-04-03 20:22:19 +0300

    [TIMETABLE-122-2] Small changes after review

--- timetable-lesson-location/src/main/resources/db/init.sql
+++ timetable-lesson-location/src/main/resources/db/init.sql
@@ -1,11 +1,11 @@
-create schema timetable_lesson_location;
-create role timetable_lesson_location_role;
+create schema lesson_location_microservice;
+create role lesson_location_role;
 
-grant connect on database pnu_system to timetable_lesson_location_role;
-grant usage on schema timetable_lesson_location to timetable_lesson_location_role;
-grant all on schema timetable_lesson_location to su_role;
+grant connect on database pnu_system to lesson_location_role;
+grant usage on schema lesson_location_microservice to lesson_location_role;
+grant all on schema lesson_location_microservice to su_role;
 
-create user timetable_lesson_location_user nosuperuser nocreatedb nocreaterole login encrypted password '12345';
-grant timetable_lesson_location_user to timetable_lesson_location_role;
+create user lesson_location_user nosuperuser nocreatedb nocreaterole login encrypted password '12345';
+grant lesson_location_user to lesson_location_role;
 
-grant select, insert, delete, update on all tables in schema timetable_lesson_location to timetable_lesson_location_role;
+grant select, insert, delete, update on all tables in schema lesson_location_microservice to lesson_location_role;
--- timetable-timetable/src/main/resources/db/init.sql
+++ timetable-timetable/src/main/resources/db/init.sql
@@ -1,11 +1,11 @@
-create schema timetable_timetable;
-create role timetable_timetable_role;
+create schema timetable_microservice;
+create role timetable_role;
 
-grant connect on database pnu_system to timetable_timetable_role;
-grant usage on schema timetable_timetable to timetable_timetable_role;
-grant all on schema timetable_timetable to su_role;
+grant connect on database pnu_system to timetable_role;
+grant usage on schema timetable_microservice to timetable_role;
+grant all on schema timetable_microservice to su_role;
 
-create user timetable_timetable_user nosuperuser nocreatedb nocreaterole login encrypted password '12345';
-grant timetable_timetable_role to timetable_timetable_user;
+create user timetable_user nosuperuser nocreatedb nocreaterole login encrypted password '12345';
+grant timetable_role to timetable_user;
 
-grant select, insert, delete, update on all tables in schema timetable_timetable to timetable_timetable_role;
+grant select, insert, delete, update on all tables in schema timetable_microservice to timetable_role;
