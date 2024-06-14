cdda6e6fddb4b33bee11b74eb8cf616d1a67b3b2
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-06-14 22:54:09 +0300

    [TIMETABLE-125] Added Lesson Location first migration with Liquibase

--- timetable-common-lib/src/main/java/com/pnu/system/common/config/TimetableCommonConfiguration.java
+++ timetable-common-lib/src/main/java/com/pnu/system/common/config/TimetableCommonConfiguration.java
@@ -1,10 +1,21 @@
 package com.pnu.system.common.config;
 
 
+import org.elasticsearch.client.sniff.Sniffer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"com.pnu.system.common.service"})
 public class TimetableCommonConfiguration {
+
+    /*
+    TODO 6/14/24: Replace with good fix, maybe...
+     U need to override with the help of Sniffer builder. If u do this, I'll buy any ice-cream for being such clever boy/girl :)
+     */
+    @Bean
+    public Sniffer sniffer() {
+        return null;
+    }
 }
