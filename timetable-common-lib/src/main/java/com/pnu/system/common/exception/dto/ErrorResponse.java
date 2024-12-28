21d404bcddead2d4ddaf0f34c4909dae704c216d
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-12-28 16:33:52 +0200

    TIMETABLE-137: Fixed

--- timetable-common-lib/src/main/java/com/pnu/system/common/exception/dto/ErrorResponse.java
+++ timetable-common-lib/src/main/java/com/pnu/system/common/exception/dto/ErrorResponse.java
@@ -1,9 +1,13 @@
 package com.pnu.system.common.exception.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Builder
 public class ErrorResponse {
 
